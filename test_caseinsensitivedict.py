26fb2970b5aec17a835db0a9b70b5f3f09ff637b
Liam Hopkins <we.hopkins@gmail.com>
2023-06-11 01:24:11 -0700

    add test_eq

--- test_caseinsensitivedict.py
+++ test_caseinsensitivedict.py
@@ -6,46 +6,63 @@ class TestCaseInsensitiveDict(TestCase):
     def test_getitem(self):
         myinstance = CaseInsensitiveDict()
         myinstance['jhin'] = 4
+        myinstance[True] = 'yes'
         self.assertEqual(myinstance['jhin'], 4)
         self.assertEqual(myinstance['JHIN'], 4)
+        self.assertEqual(myinstance[True], 'yes')
         with self.assertRaises(KeyError):
             myinstance['Liam']
 
     def test_setitem(self):
         myinstance = CaseInsensitiveDict()
         myinstance['Kaisa'] = 5
+        self.assertEqual(myinstance['Kaisa'], 5)
         myinstance['KAISA'] = 'daughter of the void'
         self.assertEqual(myinstance['Kaisa'], 'daughter of the void')
+        myinstance[True] = (0, 1)
+        self.assertEqual(myinstance[True], (0, 1))
 
     def test_list(self):
         myinstance = CaseInsensitiveDict()
         myinstance['jhin'] = 4
-        myinstance['Kaisa'] = 5
-        self.assertEqual(list(myinstance), ['jhin', 'Kaisa'])
+        myinstance[True] = 5
+        self.assertEqual(list(myinstance), ['jhin', True])
 
     def test_len(self):
         myinstance = CaseInsensitiveDict()
+        self.assertEqual(len(myinstance), 0)
         myinstance['jhin'] = 4
         myinstance['Kaisa'] = 5
         self.assertEqual(len(myinstance), 2)
+        del myinstance['jhin']
+        self.assertEqual(len(myinstance), 1)
 
     def test_del(self):
         myinstance = CaseInsensitiveDict()
         myinstance['Kaisa'] = 5
         self.assertEqual(myinstance['Kaisa'], 5)
-        del myinstance['Kaisa']
+        del myinstance['KAISA']
         self.assertNotIn('Kaisa', myinstance)
         with self.assertRaises(KeyError):
             myinstance['Kaisa']
+        with self.assertRaises(KeyError):
+            del myinstance['Kaisa']
 
     def test_keyin(self):
         myinstance = CaseInsensitiveDict()
         myinstance['Kaisa'] = 5
-        self.assertIn('Kaisa', myinstance)
+        self.assertIn('KAISA', myinstance)
+        myinstance[True] = 'yes'
+        self.assertIn(True, myinstance)
 
     def test_keynotin(self):
         myinstance = CaseInsensitiveDict()
         self.assertNotIn('Jhin', myinstance)
+        myinstance['Kaisa'] = 5
+        self.assertIn('Kaisa', myinstance)
+        self.assertNotIn('Jhin', myinstance)
+        del myinstance['Kaisa']
+        self.assertNotIn('Kaisa', myinstance)
 
     def test_clear(self):
         myinstance = CaseInsensitiveDict()
@@ -68,22 +85,18 @@ class TestCaseInsensitiveDict(TestCase):
         #     default.  If default is not given, it defaults to None, so that
         #     this method never raises a KeyError.
 
-        # items()
-        #     Return a new view of the dictionary’s items ((key, value) pairs).
-        #     See the documentation of view objects.
-
     def test_items(self):
         myinstance = CaseInsensitiveDict()
         myinstance['Kaisa'] = 5
-        myinstance['Jhin'] = 4
+        myinstance[True] = 'yes'
         self.assertIn('Kaisa', myinstance)
-        self.assertIn('Jhin', myinstance)
+        self.assertIn(True, myinstance)
         items_list = []
         for i in myinstance:
             t = (i, myinstance[i])
             items_list.append(t)
         self.assertIn(('Kaisa', 5), items_list)
-        self.assertIn(('Jhin', 4), items_list)
+        self.assertIn((True, 'yes'), items_list)
 
     def test_keys(self):
         myinstance = CaseInsensitiveDict()
@@ -115,14 +128,24 @@ class TestCaseInsensitiveDict(TestCase):
         #     If keyword arguments are specified, the dictionary is then
         #     updated with those key/value pairs: d.update(red=1, blue=2).
 
-        # values()
-        #     Return a new view of the dictionary’s values. See the documentation
-        #     of view objects.
-
     def test_values(self):
         myinstance = CaseInsensitiveDict()
         myinstance['Kaisa'] = 5
         self.assertIn('Kaisa', myinstance)
         values = myinstance.values()
-        print(f'values is {values}')
         self.assertIn(5, values)
+
+    def test_eq(self):
+        myinstance = CaseInsensitiveDict()
+        self.assertEqual(myinstance, myinstance)
+        self.assertEqual(myinstance, {})
+        myinstance['Kaisa'] = 5
+        self.assertEqual(myinstance, myinstance)
+        self.assertEqual(myinstance, {'Kaisa': 5})
+        self.assertEqual(myinstance, {'KAISA': 5})
+        self.assertNotEqual(myinstance, {'Kaisa': 4})
+        self.assertNotEqual(myinstance, {'Kaisa': '5'})
+        self.assertNotEqual(myinstance, {'Kaira': 5})
+        self.assertNotEqual(myinstance, {(0, 1): 5})
+        self.assertNotEqual(myinstance, {})
+        self.assertNotEqual(myinstance, {'Kaisa': 5, 'Jhin': 4})
